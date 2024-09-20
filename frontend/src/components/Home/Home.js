f7bcf78c2e0f8ad013a1091c3536929ffad127f4
derianlemus95 <derian.lemus95@gmail.com>
2024-09-19 19:21:30 -0700

    Frontend example of using api to retrieve user info

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,10 +1,53 @@
 import React, {Component} from "react";
 import NavigationBar from "../Navbar/Navbar";
+import axios from "axios";
+
 class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            droneDevices: [],
+            iotDevices: [],
+            cctvDevices:[],
+            users: [],
+        };
+        
+        }
+        componentDidMount(){
+        axios.get("http://localhost:8080/api/dashboard/users")
+            .then((res)=> {
+                console.log(res.data);
+                this.setState({users: res.data});
+                }).catch(error => {
+                console.error('Error getting users', error);
+               });
+        
+        //console.log("users", users)
+        }
 render() {
-    return(
+
+    const {users} = this.state;
+     return(
         <div>
-            <h1>Home Page</h1></div>
+            <h1>Home Page</h1>
+            {users.length === 0 ? (
+                <p>No Items</p>
+            ):(
+            <ul>
+                {users.map((user, index)=> (
+                    <li key={index}>
+                        <p>{user.firstName}</p>
+                        <p>{user.lastName}</p>
+                        <p>{user.email}</p>
+                        <p>{user.userId}</p>
+                        <p>{user.role}</p>
+                    </li>
+                ))}
+            </ul>
+            
+            )}
+            
+        </div>
     );
 }
 }
